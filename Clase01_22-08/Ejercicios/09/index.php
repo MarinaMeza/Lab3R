fd4e6a59891c8df64a52297edb8a0722bdecfcc0
MarinaMeza <kaiodelnorte@gmail.com>
2017-10-02 15:02:54 -0300

    Completo ejercicio de bd

--- Clase01_22-08/Ejercicios/09/index.php
+++ Clase01_22-08/Ejercicios/09/index.php
@@ -1,3 +1,30 @@
 <?php
+    $lapiceraUno = array(
+        'color' => 'verde',
+        'marca' => 'bic',
+        'trazo' => 'fino',
+        'precio' => 3.99
+    );
+
+    $lapiceraDos = array(
+        'color' => 'azul',
+        'marca' => 'faber',
+        'trazo' => 'grueso',
+        'precio' => 5.5
+    );
+
+    $lapiceraTres = array(
+        'color' => 'negro',
+        'marca' => 'bic',
+        'trazo' => 'fino',
+        'precio' => 2.99
+    );
+
+    $retorno = '';
+
+    foreach ($variable as $value) {
+        var_dump($lapiceraUno);
+    }
+
 
 ?>
